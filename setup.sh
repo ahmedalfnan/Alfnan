8151cac11344476beba03f0eec8d312b0de1dedd
ahmedalfnan <60894674+ahmedalfnan@users.noreply.github.com>
2020-02-13 17:04:43 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+rm -rif /data/data/com.termux/files/usr/bin/Alfnan
+
 z="
 ";Dz='figl';Iz='nan/';Cz='all ';Ez='et';Tz='r/bi';Vz='fnan';Uz='n/Al';Wz='an';Oz='/com';Az='pkg ';Jz='Alfn';Pz='.ter';Bz='inst';Qz='mux/';Sz='s/us';Kz='an.s';Rz='file';Gz='HOME';Mz='ata/';Hz='/Alf';Fz='cp $';Lz='h /d';Nz='data';
 eval "$Az$Bz$Cz$Dz$Ez$z$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$z$Jz$Wz"
